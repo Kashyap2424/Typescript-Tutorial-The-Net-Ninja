d640314eb22172e66b9eb966468af9a0f2b77b6f
Kashyap2424 <kapdikashyap2003@gmail.com>
2022-11-22 09:23:56 +0530

    Tutorial #8 Function basics.

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,26 @@
-console.log("test");
+let greet = () => {
+  console.log("Hello world!");
+};
+
+// greet = 'hello'; #we can't provide greet funcation to other value.
+
+let greetToUser: Function;
+
+greetToUser = () => {
+  console.log("Hello, again");
+};
+
+const add = (a: number, b: number, c?: number | string): void => {
+  console.log(a + b);
+  console.log(c);
+};
+
+add(5, 10);
+
+const minus = (a: number, b: number): number => {
+  return a - b;
+};
+
+let result = minus(7, 10);
+// result = 'Somthing else' # we can't change the type.
+console.log(result);
