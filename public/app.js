3a27016c892f70a3a3616c972e33e943c67e31a7
Kashyap2424 <kapdikashyap2003@gmail.com>
2022-11-23 08:09:05 +0530

    Tutorial #16 Interfaces with classes

--- public/app.js
+++ public/app.js
@@ -1,28 +1,21 @@
-const me = {
-    name: "kashyap",
-    age: 20,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log(`I spent ${amount}`);
-        return amount;
-    },
-};
-console.log(me);
-const greetingPerson = (person) => {
-    console.log("Hello", person.name);
-};
-greetingPerson(me);
 import { Invoice } from "./classes/invoice.js";
-const invOne = new Invoice("mario", "work on the mario website", 25000);
-const invTwo = new Invoice("mario-007", "work on the mario website", 30000);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach((inv) => {
-    console.log(inv.client, inv.amount, inv.format());
-});
+import { Payment } from "./classes/Payment.js";
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+// docOne = new Invoice("Chintan", "web work", 45000);
+// docTwo = new Payment("Maitu", "web work", 15000);
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+// console.log(docs);
+// const invOne = new Invoice("mario", "work on the mario website", 25000);
+// const invTwo = new Invoice("mario-007", "work on the mario website", 30000);
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+// invoices.forEach((inv) => {
+//   console.log(inv.client, inv.amount, inv.format());
+// });
 const form = document.querySelector(".new-item-form");
 // inputs
 const type = document.querySelector("#type");
@@ -31,5 +24,12 @@ const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
