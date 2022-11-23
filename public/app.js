ad078c296f224ebba9b1edf3344b4f76db0cce2c
Kashyap2424 <kapdikashyap2003@gmail.com>
2022-11-23 08:22:02 +0530

    Tutorial #17 Rendering an HTML template.

--- public/app.js
+++ public/app.js
@@ -1,27 +1,15 @@
 import { Invoice } from "./classes/invoice.js";
 import { Payment } from "./classes/Payment.js";
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice("Chintan", "web work", 45000);
-// docTwo = new Payment("Maitu", "web work", 15000);
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
-// const invOne = new Invoice("mario", "work on the mario website", 25000);
-// const invTwo = new Invoice("mario-007", "work on the mario website", 30000);
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-// invoices.forEach((inv) => {
-//   console.log(inv.client, inv.amount, inv.format());
-// });
+import { ListTemplate } from "./classes/ListTamplate.js";
 const form = document.querySelector(".new-item-form");
 // inputs
 const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// list template instance
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let doc;
@@ -31,5 +19,5 @@ form.addEventListener("submit", (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, "end");
 });
